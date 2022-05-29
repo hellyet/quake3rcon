d21c1ea7157d4c05d22a4704fb2d124771f1ea35
pontif1k <leonid.tosterovski@gmail.com>
2022-05-30 01:46:02 +0300

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='q3ron-py',
+    name='q3rcon-py',
+    packages=['q3rcon-py'],
     version='1.0.0',
     url='https://github.com/hellyet/q3rcon-py',
     license='MIT',
     author='hellyet',
     description='A tiny library for using Quake 3\'s RCON protocol feature for some game servers like FiveM or Quake.',
-    packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
     zip_safe=False
 )
