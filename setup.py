fd56d1403a227295f3222c47b1893d10df615eff
pontif1k <leonid.tosterovski@gmail.com>
2022-05-30 01:53:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     url='https://github.com/hellyet/q3rcon-py',
     license='MIT',
     author='hellyet',
+    author_email='leonid.tosterovski@gmail.com',
     description='A tiny library for using Quake 3\'s RCON protocol feature for some game servers like FiveM or Quake.',
     packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
