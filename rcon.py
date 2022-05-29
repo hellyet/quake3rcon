ec3acf06b9208bb072869cfa7b91de4b7d0907c3
pontif1k <leonid.tosterovski@gmail.com>
2022-05-30 00:21:51 +0300

    Add main rcon functions

--- rcon.py
+++ rcon.py
@@ -1,26 +1,45 @@
 import socket
 
 class Rcon():
-    def __init__(self, ip: str, port: int, password: int) -> None:
-        
+    def __init__(self, ip: str, port: int, password: int, timeout: float = 0.7) -> None:
+        """Initialization of rcon connection
+
+        Args:
+            ip (str): Server ip adress
+            port (int): Rcon port
+            password (int): Rcon password
+            timeout (float, optional): Defaults to 0.7.
+        """
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.ip = ip
         self.port = port
         self.password = password 
         
         self.socket.connect((self.ip, self.port))
-        self.socket.settimeout(0.7)
+        self.socket.settimeout(timeout)
         
         
     def __del__(self) -> None:
+        "Close rcon connection on object destruction"
         self.socket.close()
     
     
     def recv(self):
+        "Gets server response"
         return self.socket.recv(8192)
     
     
     def send(self, command: str) -> str:
+        """Send rcon command
+
+        Args:
+            command (str): Rcon command to send
+
+        Returns:
+            str: Rcon responce
+        """
+        
         buffer = 0xFFFFFFFF.to_bytes(4, byteorder='big') # magic code
         buffer += bytes('rcon ', 'utf-8')
         buffer += bytes(self.password, 'utf-8')
@@ -32,6 +51,7 @@ class Rcon():
             respoce = self.recv()
         except Exception:
             respoce = None
+            
         if respoce :
             if str(respoce[4:])[:8] == "b'print " and str(respoce[4:])[8:-3] != "": return str(respoce[4:])[8:-3].replace('\\n','\n')
             else:  return 0
